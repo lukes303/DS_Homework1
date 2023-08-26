2a96e7ed0df337fab9229a053edbc90b6f6b86cb
Luke Selby <lukeselby3@gmail.com>
2023-08-26 12:01:30 -0500

    Finished task 1- 2

--- hw_1_C++.cpp
+++ hw_1_C++.cpp
@@ -63,37 +63,40 @@ private:
 	float GPA;  // It holds a student's GPA.
 };
 
-//Start of my implementations for task 2
+//Student Implementations
+
 void Student::set_SID(int x){
-	Student::SID = x;
+	SID = x;
 }
 
 void Student::prt_SID(){
-	cout << Student::SID;
+	cout << SID;
 }
 
 void Student::set_GPA(float y){
-	Student::GPA = y;
+	GPA = y;
 }
 
 void Student::prt_GPA(){
-	cout << Student::GPA;
+	cout << GPA;
 }
 
 void Student::reset(){
-	Student::SID = -1;
-	Student::GPA = -1;
+	set_SID(-1.0);
+	set_GPA(-1.0);
 }
 
 float Student::get_GPA(){
-	return Student::GPA;
+	return GPA;
 }
 
 Student::Student(){
-	Student::SID = -1;
-	Student::GPA = -1;
+	SID = -1.0;
+	GPA = -1.0;
 }
-//End of task 2 implementations
+
+//End of Student Implementations
+
 
 class University {
 public:
@@ -110,6 +113,76 @@ private:
 //......
 //......
 //......
+
+//University Implementations
+
+void University::set_Stu(Student x[]){
+	
+	for(int i = 0; i < 5; i++){
+		Sooner[i] = x[i];
+	}
+}
+
+float University::GPA_Mean(){
+
+	float gpaSum = 0;
+	float gpaMean;
+
+	for(int i = 0; i < 5; i++){
+		gpaSum += Sooner[i].get_GPA();
+	}
+
+	gpaMean = gpaSum / 5;
+
+	cout << gpaMean;
+
+	return gpaMean;
+}
+
+float University::GPA_Max(){
+	
+	float gpaMax = University::Sooner[0].get_GPA();
+
+	for(int i = 1; i < 5; i++){
+		
+		if(University::Sooner[i].get_GPA() > gpaMax){
+			gpaMax = University::Sooner[i].get_GPA();
+		}
+	}
+
+	cout << gpaMax;
+
+	return gpaMax;
+}
+
+float University::GPA_Min(){
+	
+	float gpaMin = University::Sooner[0].get_GPA();
+
+	for(int i = 1; i < 5; i++){
+		
+		if(University::Sooner[i].get_GPA() < gpaMin){
+			gpaMin = University::Sooner[i].get_GPA();
+		}
+	}
+
+	cout << gpaMin;
+
+	return gpaMin;
+}
+
+University::University(){
+
+	for(int i = 0; i < 5; i++){
+
+		University::Sooner[i] = Student();
+	}
+}
+
+//End of University implementations
+
+
+
 // === End of Task 2 ====
 
 
@@ -157,20 +230,20 @@ int main()
 
 	// ======= Test 3 =======
 	// This tests your University class. 
-	OU.set_Stu(x);
-	OU.GPA_Mean(); cout << ' ';
-	OU.GPA_Max();  cout << ' ';
-	OU.GPA_Min();  cout << endl;
+	// OU.set_Stu(x);
+	// OU.GPA_Mean(); cout << ' ';
+	// OU.GPA_Max();  cout << ' ';
+	// OU.GPA_Min();  cout << endl;
 	// === End of Test 3 ====
 
 
 	// ======= Test 4 =======
 	// This tests your reset function.
-	for (int i = 0; i < 5; i++) {
-		x[i].reset();
-		x[i].prt_SID();	cout << ' ';
-		x[i].prt_GPA();	cout << endl;
-	}
+	// for (int i = 0; i < 5; i++) {
+	// 	x[i].reset();
+	// 	x[i].prt_SID();	cout << ' ';
+	// 	x[i].prt_GPA();	cout << endl;
+	// }
 	// === End of Test 4 ====
 	
 	return 0;
